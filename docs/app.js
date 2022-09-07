45ddfef55439df531b99787bb51cfeb4768bdf42
Brandon <brandonbiera@hotmail.com>
2022-09-07 00:23:28 -0400

    Now playing modal
    -- Added new now playing modal
    -- Clicking the currently playing widget opens the now playing modal
    -- Added song rating
    -- Added previous and next song buttons
    -- Added play/pause button

--- docs/app.js
+++ docs/app.js
@@ -1,6 +1,12 @@
 let options = new HydrateAppOptions();
 options.debug.dispatchTimer = false;
 let hydrate = new HydrateApp(options);
+let appState = {
+    playback: null,
+    songs: null,
+    tabs: null
+};
+let model = hydrate.bind("model", appState);
 let songs = [];
 for (let i = 0; i < 20; i++)
     songs.push({
@@ -8,11 +14,13 @@ for (let i = 0; i < 20; i++)
         name: `Song ${i}`,
         artists: `Artist ${i}`,
         albumCover: "https://upload.wikimedia.org/wikipedia/en/1/1b/Issues_Headspace.jpg",
+        rating: 3
     });
-let model = hydrate.bind("model");
 model.songs = songs;
 model.playback = {
-    song: null //songs[0]
+    song: null,
+    modal: false,
+    playing: false,
 };
 let tabs = [
     {
@@ -45,4 +53,31 @@ model.tabs = tabs;
 hydrate.route();
 function playSong(song) {
     model.playback.song = song;
+    togglePlay(true);
+}
+function togglePlay(toggle) {
+    if (toggle == null)
+        toggle = !model.playback.playing;
+    model.playback.playing = toggle;
+    model.playback.playing = toggle;
+}
+function nextSong() {
+    let index = appState.songs.indexOf(appState.playback.song);
+    if (index < 0)
+        return;
+    index = (index + 1) % appState.songs.length;
+    playSong(appState.songs[index]);
+}
+function previousSong() {
+    let index = appState.songs.indexOf(appState.playback.song);
+    if (index < 0)
+        return;
+    index = (index - 1 + appState.songs.length) % appState.songs.length;
+    playSong(appState.songs[index]);
+}
+function rankSong(songModel, ranking) {
+    songModel.rating = ranking;
+}
+function viewNowPlaying(toggle) {
+    model.playback.modal = toggle ?? !model.playback.modal;
 }
--- docs/app.ts
+++ docs/app.ts
@@ -1,12 +1,24 @@
 let options = new HydrateAppOptions();
 options.debug.dispatchTimer = false;
 let hydrate = new HydrateApp(options);
+interface AppState {
+    songs:SongListItem[];
+    playback:PlayerPlayback;
+    tabs:AppNavbarTab[];
+}
+let appState:AppState = {
+    playback: null,
+    songs: null,
+    tabs: null
+};
+let model:AppState = hydrate.bind("model", appState);
 
 interface SongListItem {
     id:string;
     name:string;
     artists:string;
     albumCover:string;
+    rating:number;
 }
 
 let songs:SongListItem[] = [];
@@ -16,19 +28,25 @@ for(let i = 0; i < 20; i++)
         name: `Song ${i}`,
         artists: `Artist ${i}`,
         albumCover: "https://upload.wikimedia.org/wikipedia/en/1/1b/Issues_Headspace.jpg",
+        rating: 3
     });
-let model = hydrate.bind("model");
+model.songs = songs;
+interface PlayerPlayback {
+    song:SongListItem;
+    modal:boolean;
+    playing:boolean;
+}
+model.playback = {
+    song: null,//songs[0],
+    modal: false,//false
+    playing: false,
+}
 
 interface AppNavbarTab {
     name:string;
     link:string;
     icon:string;
 }
-model.songs = songs;
-model.playback = {
-    song: null//songs[0]
-}
-
 let tabs:AppNavbarTab[] = [
     {
         name: "Discover",
@@ -62,4 +80,32 @@ hydrate.route();
 
 function playSong(song:SongListItem) {
     model.playback.song = song;
+    togglePlay(true);
+}
+function togglePlay(toggle?:boolean) {
+    if(toggle == null)
+        toggle = !model.playback.playing;
+    model.playback.playing = toggle;
+    model.playback.playing = toggle;
+}
+function nextSong() {
+    let index = appState.songs.indexOf(appState.playback.song);
+    if(index < 0)
+        return;
+    index = (index + 1) % appState.songs.length;
+    playSong(appState.songs[index]);
+}
+function previousSong() {
+    let index = appState.songs.indexOf(appState.playback.song);
+    if(index < 0)
+        return;
+    index = (index - 1 + appState.songs.length) % appState.songs.length;
+    playSong(appState.songs[index]);
+}
+function rankSong(songModel:SongListItem, ranking:number) {
+    songModel.rating = ranking;
+}
+
+function viewNowPlaying(toggle?:boolean) {
+    model.playback.modal = toggle ?? !model.playback.modal;
 }
